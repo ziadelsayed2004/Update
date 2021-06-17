bb224f4210c2b2b90f68061463d9ce16ffd898a5
OmarShawkey13 <81522507+OmarShawkey13@users.noreply.github.com>
2021-06-17 02:19:52 +0200

    Add files via upload

---
+++ osmods.js
@@ -0,0 +1,14 @@
+{
+	"version": "7.0",
+	"download": "https://ymmods-official.blogspot.com/2021/04/ymwab.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-B-es.html"
+		}
+	]
+}
