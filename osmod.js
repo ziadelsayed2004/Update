c0f2c57ec085e9732e492e943f0123c6fbfda911
OmarShawkey13 <81522507+OmarShawkey13@users.noreply.github.com>
2021-06-19 10:28:13 +0200

    Add files via upload

---
+++ osmod.js
@@ -0,0 +1,14 @@
+{
+	"version": "1.0",
+	"download": "https://osmodwa.blogspot.com/2021/05/whatsapp-store-base-2211016-with-privacy.html?m=1",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/OmarShawkey13/Update/main/os-update.html"
+		},
+		{
+			"languaje": "ar",
+			"changelog": "https://raw.githubusercontent.com/OmarShawkey13/Update/main/os-update-2.html"
+		}
+	]
+}
