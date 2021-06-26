162aab62b2189d48895ad267f4147009b3e7cb95
OmarShawkey13 <81522507+OmarShawkey13@users.noreply.github.com>
2021-06-26 16:10:09 +0200

    Update osmod.js

--- osmod.js
+++ osmod.js
@@ -1,6 +1,6 @@
 {
-	"version": "1.0",
-	"download": "http://osmodwa.blogspot.com/2021/06/whatsapp-business-store-base-2211112.html",
+	"version": "2.0",
+	"download": "https://osmodwa.blogspot.com/2021/06/whatsapp-business-store-base-2211112.html?m=1",
 	"update": [
 		{
 			"languaje": "en",
